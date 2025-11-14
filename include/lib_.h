f85eb47f0b0fe83ac35bf333b517a5b927e63fc1
gauravashisth <miya.musashi.moto1645@gmail.com>
2025-11-14 23:55:50 +0530

    feat: add custom string compare function, strcmp_

---
+++ include/lib_.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define _GNU_SOURCE
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int strcmp_(char *s1, char *s2);
---
+++ test/strcmp_.c
@@ -0,0 +1,7 @@
+#include "lib_.h"
+
+int main() {
+  strcmp_("abc", "abc");
+  printf("Event loop test passed\n");
+  return EXIT_SUCCESS;
+}
